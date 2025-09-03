3ba24f713cef970c6aa644dc0c10f681baec426e
jacob <jacobhwei@outlook.com>
2025-09-03 13:21:20 -0700

    finalizing nav bar

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -6,17 +6,45 @@ export const MobileNav = ({ menuOpen, setMenuOpen }) => {
       className={`fixed top-0 left-0 w-full bg-[rgba(10,10,10,0.65)]
       z-40 flex flex-col items-center justify-around
       transition-all duration-300 ease-in-out
-      ${
-        menuOpen
-          ? "h-screen opacity-100 pointer-events-auto"
-          : "h-0 opacity-0 pointer-events-none"
-      }`}
+      ${menuOpen ? "h-screen opacity-100 pointer-events-auto" : "h-0 opacity-0 pointer-events-none"}`}
     >
-      <button onClick={() => setMenuOpen(false)}
+      <button
+        onClick={() => setMenuOpen(false)}
         className="absolute top-6 right-6 text-white text-3xl focus:outline-none cursor-pointer"
+        aria-label="Close Menu"
       >
         &times;
       </button>
+      <a
+        href="#Home"
+        onClick={() => setMenuOpen(false)}
+        className="font-semibold text-white my-4 transform transition-transform duration-200 font-Rubik"
+      >
+        Home
+      </a>
+      <a
+        href="#About"
+        onClick={() => setMenuOpen(false)}
+        className={`font-semibold text-white my-4 transform transition-transform duration-200 font-Rubik ${
+          menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"
+        }`}
+      >
+        About
+      </a>
+      <a
+        href="#Education"
+        onClick={() => setMenuOpen(false)}
+        className="font-semibold text-white my-4 transform transition-transform duration-200 font-Rubik"
+      >
+        Education
+      </a>
+      <a
+        href="#Projects"
+        onClick={() => setMenuOpen(false)}
+        className="font-semibold text-white my-4 transform transition-transform duration-200 font-Rubik"
+      >
+        Projects
+      </a>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,42 @@
-import { useEffect } from "react"
+import { useEffect } from "react";
 
-export const Navbar = ({menuOpen, setMenuOpen}) => {
+export const Navbar = ({ menuOpen, setMenuOpen }) => {
+  useEffect(() => {
+    document.body.style.overflow = menuOpen ? "hidden" : "auto";
+  }, [menuOpen]);
 
-    useEffect(() => {
-        document.body.style.overflow = menuOpen ? "hidden" : "auto";
-    }, [menuOpen]);
-
-    return (
-        <nav className="fixed top-0 w-full z-40 bg-[rgba(10,10,10,0.65)]
-        backdrop-blur-lg border-b border-white shadow-lg"> 
-            <div className="max-w-5xl mx-auto px-4">
-                <div className="flex justify-between items-center h-16">
-                    <a href="#home" className="font-mono text-xl font-bold text-white"> {""}
-                        Jacob<span className="text-blue-400"> Wei</span>{""}
-                    </a> 
-                    <div className="w-7 h-5 relative cursor-pointer x-40 md:hidden" 
-                    onClick={() => setMenuOpen((prev)=>!prev)}>
-                        &#9776;
-                    </div>
-                    <div className="hidden md:flex items-center space-x-8">
-                        <a href = "home" className="text-white transition colors">
-                            {""}Home{""}
-                        </a>
-                        <a href = "About" className="text-white transition colors">
-                            {""}About{""}
-                        </a>
-                        <a href = "Education" className="text-white transition colors">
-                            {""}Education{""}
-                        </a>
-                        <a href = "Projects" className="text-white transition colors">
-                            {""}Projects{""}
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    );
+  return (
+    <nav
+      className="fixed top-0 w-full z-40 bg-[rgba(10,10,10,0.65)]
+      backdrop-blur-lg border-b border-white shadow-lg"
+    >
+      <div className="max-w-5xl mx-auto px-4">
+        <div className="flex justify-between items-center h-16">
+          <a href="#Home" className="font-bold text-xl text-white font-Rubik">
+            Jacob<span className="text-blue-400"> Wei</span>
+          </a>
+          <div
+            className="w-7 h-5 relative cursor-pointer md:hidden"
+            onClick={() => setMenuOpen((prev) => !prev)}
+          >
+            &#9776;
+          </div>
+          <div className="hidden md:flex items-center space-x-7 font-Rubik">
+            <a href="#Home" className="font-bold text-white font-Rubik">
+              Home
+            </a>
+            <a href="#About" className="font-bold text-white font-Rubik">
+              About
+            </a>
+            <a href="#Education" className="font-bold text-white font-Rubik">
+              Education
+            </a>
+            <a href="#Projects" className="font-bold text-white font-Rubik">
+              Projects
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
